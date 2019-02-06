900a0311e57feb3038a5a83f05d5d99497ade85e
sarahnash <sarahnash1112@gmail.com>
2019-02-05 21:24:18 -0600

    exercises complete, working on bonus

--- src/index.js
+++ src/index.js
@@ -10,3 +10,26 @@ subscribe(() => render(getState()))
 // Dispatch the "INCREMENT" action every time the +1 button is pressed
 const incrementButton = document.getElementById('increment');
 incrementButton.addEventListener('click', e => dispatch({ type: "INCREMENT" }));
+
+// Dispatch the "DECREMENT" action every time the -1 button is pressed
+const decrementButton = document.getElementById('decrement');
+decrementButton.addEventListener('click', e => dispatch({ type: "DECREMENT" }));
+
+// Dispatch the "PLUSFIVE" action every time the +5 button is pressed
+const plusFiveButton = document.getElementById('plusfive');
+plusFiveButton.addEventListener('click', e => dispatch({ type: "ADD", value: 5 }));
+
+// Dispatch the "MINUSFIVE" action every time the -5 button is pressed
+const minusFiveButton = document.getElementById('minusfive');
+minusFiveButton.addEventListener('click', e => dispatch({ type: "ADD", value: -5 }));
+
+// Dispatch the "coloroption" action every time a color option is selected
+const colorOption = document.getElementById('coloroption');
+colorOption.addEventListener('change', e => dispatch({ type: "COLOROPTION" }));
+
+// Dispatch the "userinput" action every time a user submits text
+const addCounter = document.getElementById('addcounter');
+addcounter.addEventListener('click', e => dispatch({ type: "ADDCOUNTER" }));
+
+// force an initial render
+dispatch({type: 'INIT'})
--- src/reducer.js
+++ src/reducer.js
@@ -1,12 +1,39 @@
 const initialState = {
-    value: 0
+    value: 0,
+    color: 'red'
 }
 
 const reducer = (state = initialState, action) => {
     let { type } = action;
     if (type === "INCREMENT") {
         return {
-            value: state.value + 1
+            value: state.value + 1,
+            color: state.color
+        }
+    } else if (type === "DECREMENT") {
+        return {
+            value: state.value - 1,
+            color: state.color
+        }
+    } else if (type === "ADD") {
+        return {
+            value: state.value + action.value,
+            color: state.color
+        }
+    } else if (type === "COLOROPTION") {
+        return {
+            value: state.value,
+            color: state.color = colorOption.value
+        }
+    } else if (type === "USERINPUT") {
+        return {
+            value: parseInt(userInput.value),
+            color: state.color
+        }
+    } else if (type === "ADDCOUNTER") {
+        return {
+            value: state.value,
+            color: state.color
         }
     }
     return state
